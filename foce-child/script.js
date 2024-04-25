91187476552a16f26d80e987ea65a781c6fafa91
Antoine-Thomas <searching.murphy@gmail.com>
2024-04-25 18:03:56 +0200

    Update script.js

--- foce-child/script.js
+++ foce-child/script.js
@@ -1,5 +1,6 @@
 // Attend que le DOM soit chargé avant d'exécuter le script
 document.addEventListener("DOMContentLoaded", function () {
+    // Sélecteurs
     const sections = document.querySelectorAll("section");
     const videoEl = document.querySelector('.videolog');
     const menuToggle = document.querySelector('.menu-toggle');
@@ -15,22 +16,16 @@ document.addEventListener("DOMContentLoaded", function () {
     const body = document.querySelector('body');
     const burgerMenuContainer = document.querySelector('.burger-menu-container');
 
-
-    // Fonction pour réinitialiser l'animation des titres
+    // Fonctions pour animer les titres
     function resetTitleAnimation(titleElement) {
         titleElement.style.opacity = "0";
         titleElement.style.transform = "translateY(20px)";
     }
 
-    // Fonction pour animer les titres
     function animateTitle(titleElement) {
         titleElement.style.opacity = "1";
         titleElement.style.transform = "translateY(0)";
     }
-
-
-
-
 
     // Fonction pour mettre à jour la position du logo en fonction du défilement de la page
     function updateLogoPosition() {
@@ -47,56 +42,28 @@ document.addEventListener("DOMContentLoaded", function () {
             logoPosition = Math.min(scrollPosition, maxScrollDistance);
         }
 
-        // Vérifie si l'utilisateur fait défiler vers le bas sur un écran de 920px ou moins
         if (scrollPosition > 0 && screenWidth <= 920) {
-            // Si l'utilisateur fait défiler vers le bas et l'écran est de 920px ou moins
-            logo.style.top = `${window.innerHeight / 300}px`; // Positionne le logo au centre vertical de la fenêtre
+            logo.style.top = `${window.innerHeight / 300}px`;
         } else {
-            // Sinon, le logo suit la logique de défilement normale
             logo.style.top = `${50 + logoPosition}px`;
         }
     }
 
-
-
-    // Écoute les événements de défilement de la page pour mettre à jour la position du logo
-    window.addEventListener('scroll', updateLogoPosition);
-
-    // Ajoute une classe 'loaded' à la vidéo après un délai pour activer les styles CSS
-    setTimeout(() => videoEl.classList.add('loaded'), 500);
-
-    // Variable pour suivre le clic sur le menu toggle
-    let menuToggleClicked = false;
-
-        // Fonction pour afficher/masquer le menu burger
-        function toggleBurgerMenu() {
-            const burgerMenuContainer = document.querySelector('.burger-menu-container');
-    
-            if (burgerMenuContainer.style.display === 'none' || burgerMenuContainer.style.display === '') {
-                burgerMenuContainer.style.display = 'block';
-                burgerMenuContainer.classList.add('active');
-                menuToggle.classList.add('active');
-                body.classList.add('no-scroll'); // Ajoute la classe 'no-scroll' au body
-            } else {
-                burgerMenuContainer.style.display = 'none';
-                burgerMenuContainer.classList.remove('active');
-                menuToggle.classList.remove('active');
-                body.classList.remove('no-scroll'); // Supprime la classe 'no-scroll' du body
-            }
-            menuToggleClicked = true;
+    // Fonction pour afficher/masquer le menu burger
+    function toggleBurgerMenu() {
+        if (burgerMenuContainer.style.display === 'none' || burgerMenuContainer.style.display === '') {
+            burgerMenuContainer.style.display = 'block';
+            burgerMenuContainer.classList.add('active');
+            menuToggle.classList.add('active');
+            body.classList.add('no-scroll');
+        } else {
+            burgerMenuContainer.style.display = 'none';
+            burgerMenuContainer.classList.remove('active');
+            menuToggle.classList.remove('active');
+            body.classList.remove('no-scroll');
         }
-    
-        // Gestionnaires d'événements pour le menu burger
-        menuToggle.addEventListener('click', toggleBurgerMenu);
-        closeMenu.addEventListener('click', toggleBurgerMenu);
-        window.addEventListener('click', function(event) {
-            if (event.target !== burgerMenu && !menuToggleClicked) {
-                burgerMenu.style.display = 'none';
-                menuToggle.classList.remove('active');
-                body.classList.remove('no-scroll'); // Supprime la classe 'no-scroll' du body
-            }
-            menuToggleClicked = false;
-        });
+        menuToggleClicked = true;
+    }
 
     // Fonction pour détecter quelle section de la page est visible lors du défilement
     function detectVisibleSection() {
@@ -105,78 +72,91 @@ document.addEventListener("DOMContentLoaded", function () {
             const isVisible = rect.top >= 0 && rect.top <= window.innerHeight;
 
             if (isVisible) {
-                // Ajoute une classe 'active' à la section visible
                 section.classList.add("active");
                 if (section.id === "character-section") {
-                    animateTitle(characterTitle); // Anime le titre des personnages
+                    animateTitle(characterTitle);
                 } else if (section.id === "lelieu") {
-                    animateTitle(locationTitle); // Anime le titre du lieu
-                    locationContainer.classList.add("active"); // Ajoute la classe 'active' au conteneur du lieu
+                    animateTitle(locationTitle);
+                    locationContainer.classList.add("active");
                 } else if (section.id === "story") {
-                    animateTitle(histoireText); // Anime le texte de l'histoire
+                    animateTitle(histoireText);
                 }
             } else {
-                // Supprime la classe 'active' de la section non visible
                 section.classList.remove("active");
                 if (section.id === "character-section") {
-                    resetTitleAnimation(characterTitle); // Réinitialise l'animation du titre des personnages
+                    resetTitleAnimation(characterTitle);
                 } else if (section.id === "lelieu") {
-                    resetTitleAnimation(locationTitle); // Réinitialise l'animation du titre du lieu
+                    resetTitleAnimation(locationTitle);
                 } else if (section.id === "story") {
-                    resetTitleAnimation(histoireText); // Réinitialise l'animation du texte de l'histoire
+                    resetTitleAnimation(histoireText);
                 }
             }
         });
+    }
 
+    // Fonction pour détecter la visibilité de la section du titre personnages
+    function detectVisibleCharacterSection() {
+        const rect = characterArticle.getBoundingClientRect();
+        const isVisible = rect.top >= 0 && rect.top <= window.innerHeight;
 
-        // Fonction pour détecter la visibilité de la section du titre personnages
-        function detectVisibleCharacterSection() {
-            const rect = characterArticle.getBoundingClientRect();
-            const isVisible = rect.top >= 0 && rect.top <= window.innerHeight;
-
-            if (isVisible) {
-                characterArticle.classList.add("active");
-                animateTitle(characterTitle);
-            } else {
-                characterArticle.classList.remove("active");
-                resetTitleAnimation(characterTitle);
-            }
+        if (isVisible) {
+            characterArticle.classList.add("active");
+            animateTitle(characterTitle);
+        } else {
+            characterArticle.classList.remove("active");
+            resetTitleAnimation(characterTitle);
         }
+    }
 
+    // Fonction pour détecter la visibilité de la section du lieu
+    function detectVisibleLieuSection() {
+        const rect = lieuArticle.getBoundingClientRect();
+        const isVisible = rect.top >= 0 && rect.top <= window.innerHeight;
 
-
-
-        // Fonction pour détecter la visibilité de la section du lieu
-
-        function detectVisibleLieuSection() {
-            const rect = lieuArticle.getBoundingClientRect();
-            const isVisible = rect.top >= 0 && rect.top <= window.innerHeight;
-
-            if (isVisible) {
-                lieuArticle.classList.add("active");
-                animateTitle(locationTitle);
-                locationContainer.classList.add("active");
-            } else {
-                lieuArticle.classList.remove("active");
-                resetTitleAnimation(locationTitle);
-                locationContainer.classList.remove("active");
-            }
-        }
-
-        // Écoute les événements de défilement de la page pour détecter la visibilité de la section du lieu
-        window.addEventListener('scroll', detectVisibleLieuSection);
-        window.addEventListener('scroll', detectVisibleCharacterSection);
-
-        // Supprime la classe 'active' du conteneur du lieu si la section du lieu n'est pas visible
-        if (!locationContainer.classList.contains("active")) {
+        if (isVisible) {
+            lieuArticle.classList.add("active");
+            animateTitle(locationTitle);
+            locationContainer.classList.add("active");
+        } else {
+            lieuArticle.classList.remove("active");
+            resetTitleAnimation(locationTitle);
             locationContainer.classList.remove("active");
         }
     }
 
-    // Écoute les événements de défilement de la page pour détecter la visibilité des sections
-    window.addEventListener('scroll', detectVisibleSection);
+    // Fonction pour déplacer les nuages en fonction du défilement de la page
+    function moveCloudsByScroll() {
+        const lieu = document.getElementById("lelieu");
+        const rootElement = document.documentElement;
+        let scrollPosition = window.scrollY - lieu.offsetTop;
+        let cloudPosition = Math.round(scrollPosition / 3);
+        if (cloudPosition >= 1 && cloudPosition <= 1500) {
+            rootElement.style.setProperty("--posX", `-${cloudPosition}px`);
+        }
+    }
 
-    // Initialise le carrousel des personnages avec l'effet Cover Flow de SwiperJS
+    // Événements
+    window.addEventListener('scroll', updateLogoPosition);
+    window.addEventListener('scroll', detectVisibleSection);
+    window.addEventListener('scroll', detectVisibleLieuSection);
+    window.addEventListener('scroll', detectVisibleCharacterSection);
+    window.addEventListener('scroll', moveCloudsByScroll);
+
+    menuToggle.addEventListener('click', toggleBurgerMenu);
+    closeMenu.addEventListener('click', toggleBurgerMenu);
+    window.addEventListener('click', function (event) {
+        if (event.target !== burgerMenu && !menuToggleClicked) {
+            burgerMenu.style.display = 'none';
+            menuToggle.classList.remove('active');
+            body.classList.remove('no-scroll');
+        }
+        menuToggleClicked = false;
+    });
+
+    setTimeout(() => videoEl.classList.add('loaded'), 500);
+
+    let menuToggleClicked = false;
+
     const swiperEl = document.querySelector('.mySwiper');
     if (swiperEl) {
         new Swiper(".mySwiper", {
@@ -201,33 +181,19 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     }
 
-    // Fonction pour déplacer les nuages en fonction du défilement de la page
-    function moveCloudsByScroll() {
-        const lieu = document.getElementById("lelieu");
-        const rootElement = document.documentElement;
-        let scrollPosition = window.scrollY - lieu.offsetTop;
-        let cloudPosition = Math.round(scrollPosition / 3);
-        if (cloudPosition >= 1 && cloudPosition <= 1500) {
-            rootElement.style.setProperty("--posX", `-${cloudPosition}px`);
-        }
+    if (!locationContainer.classList.contains("active")) {
+        locationContainer.classList.remove("active");
     }
-
-    // Écoute les événements de défilement de la page pour déplacer les nuages
-    window.addEventListener('scroll', moveCloudsByScroll);
-
-  
-
-
 });
 
 // Fonction jQuery pour animer les éléments avec la classe 'fade-in'
 jQuery(document).ready(function ($) {
     $('.fade-in').each(function () {
-        $(this).css('opacity', 0) // Définit l'opacité initiale à 0 pour masquer les éléments
-            .delay(250) // Délai de 2 secondes avant le début du fade-in
+        $(this).css('opacity', 0)
+            .delay(250)
             .animate({
-                opacity: 1 // Fait apparaître progressivement les éléments
-            }, 800); // Durée du fade-in sur 2 secondes
+                opacity: 1
+            }, 800);
     });
 });
 
