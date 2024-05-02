077ac50dc3e455d03a2bf7345b9de1f4c4b25992
Antoine-Thomas <searching.murphy@gmail.com>
2024-05-02 16:50:46 +0200

    menu burger avec chats

--- foce-child/front-page.php
+++ foce-child/front-page.php
@@ -69,6 +69,7 @@
             <p>Avec une créativité et une capacité d'innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente "Fleurs d'oranger et chats errants".</p>
         </div>
     </section>
+    
 </main>
 
 <?php get_footer(); ?>
